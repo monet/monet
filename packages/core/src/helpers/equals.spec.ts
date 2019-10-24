8ea24708b87d1b80c22a00ed886b5fe3cfc0ffe6
Jakub Strojewski <jakub.strojewski@allegro.pl>
2019-10-24 18:36:07 +0200

    Upgrade tests [ #9 ]

---
+++ packages/core/src/helpers/equals.spec.ts
@@ -0,0 +1,79 @@
+import { expect } from 'chai'
+
+import { feature, given, scenario, then, when } from '../testing'
+import { equals } from './equals'
+
+feature`equals`(() => {
+
+  scenario`testing equal values`(() => {
+    let equalPairs: [any, any][]
+    let results: boolean[]
+
+    given`a set of pairs of equal values`(() => {
+      const o = {}
+      const a = []
+      equalPairs = [
+        [true, true],
+        ['Equal string', 'Equal string'],
+        [0, 0],
+        [10, 10],
+        [NaN, NaN],
+        [null, null],
+        [undefined, undefined],
+        [o, o],
+        [a, a],
+      ]
+    })
+
+    when`values from a pair are matched` (() => {
+      results = equalPairs.map(([a, b]) => equals(a)(b))
+    })
+
+    then`they pass the test`(() => {
+      expect(results).to.deep.equal([true, true, true, true, true, true, true, true, true])
+    })
+
+  })
+
+  scenario`testing not equal values`(() => {
+    let notEqualPairs: [any, any][]
+    let results: boolean[]
+
+    given`a set of pairs of equal values`(() => {
+      notEqualPairs = [
+        [true, false],
+        ['yes', 'no'],
+        [0, 1],
+        [3, '3'],
+        [3, [3]],
+        [NaN, 0],
+        [NaN, ''],
+        [null, undefined],
+        [{}, {}],
+        [[], []],
+        [[], {}],
+      ]
+    })
+
+    when`values from a pair are matched` (() => {
+      results = notEqualPairs.map(([a, b]) => equals(a)(b))
+    })
+
+    then`they do not pass the test`(() => {
+      expect(results).to.deep.equal(
+        [false, false, false, false, false, false, false, false, false, false, false])
+    })
+
+  })
+
+  // given`Not equal values expect result to be false`(() => {
+  //   expect(equals(true)(false)).to.equal(false)
+  //   expect(equals(0)(1)).to.equal(false)
+  //   expect(equals('yes')('no')).to.equal(false)
+  //   expect(equals(false)(true)).to.equal(false)
+  //   expect(equals([])({})).to.equal(false)
+  //   expect(equals({})({})).to.equal(false)
+  //   expect(equals(3)([3])).to.equal(false)
+  //   expect(equals(3)('3')).to.equal(false)
+  // })
+})
--- packages/core/src/testing/ghrkn.spec.ts
+++ packages/core/src/testing/ghrkn.spec.ts
@@ -2,26 +2,20 @@ import { expect } from 'chai'
 
 import { and, feature, given, scenario, then, when } from './ghrkn'
 
-feature `Gherkin style helpers` (() => {
+feature`Gherkin style helpers`(() => {
 
-  scenario `Testing testing utils` (() => {
+  scenario`Testing testing utils`(() => {
 
-    given `all testing helpers` (() => {
-
-      expect(and).to.not.be.undefined // tslint:disable-line:no-unused-expression
-
+    given`all testing helpers`(() => {
+      expect(and).to.not.be.undefined
     })
 
-    when `tests are run` (() => {
-
+    when`tests are run`(() => {
       expect(and).to.be.instanceOf(Function)
-
     })
 
-    then `everything is tested` (() => {
-
+    then`everything is tested`(() => {
       expect(1).to.equal(1)
-
     })
 
   })
